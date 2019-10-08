827ae50d7227cebc24e74bb72707c9fd6d93703a
yagippi27 <an.suhyun927@gmail.com>
2019-10-08 22:10:59 +0900

    ARIMA and SMAPE

--- SMAPE.py
+++ SMAPE.py
@@ -4,13 +4,19 @@
 # In[2]:
 
 
-## anaconda에서 numba를 찾은 후, 설치해 주세요.
-
-# @jit
-def smape_fast(y_true, y_pred):
+def smape_fast(df_y_true, df_y_pred):
+    """
+    @jit
+    using SMAPE(Symmetric mean absolute percentage error)
+    :setting:install 'numba' in anaconda
+    :df_y_true: Real DateFrame
+    :df_y_pred: DataFrame you made    
+    """
     from numba import jit
     import math
     out = 0
+    y_true = df_y_true.drop(df_y_true.columns[0], axis=1).values.reshape(-1)
+    y_pred = df_y_pred.drop(df_y_pred.columns[0], axis=1).values.reshape(-1)    
     for i in range(y_true.shape[0]):
         a = y_true[i]
         b = y_pred[i]
